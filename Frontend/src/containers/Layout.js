2891df83c7167612602f08b4c9695e0d58e4c003
Amal_Ahmed <Amool_Shafeeq19@Yahoo.com>
2019-11-20 22:56:59 +0300

    components has been added

--- Frontend/src/containers/Layout.js
+++ Frontend/src/containers/Layout.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';	{/*Importing important layout from antd UI Framework*/}
 
 
-
 const { Header, Content, Footer } = Layout;
 
 class CustomLayout extends React.Component {
   render() {
 	return (
 		<Layout>
-    {/*Beginning Of Header layout*/}
+    {/*Beginning of Header layout*/}
 		<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
 			<div className="logo" />
 			<Menu
 				theme="dark"
 				mode="horizontal"
-				defaultSelectedKeys={['1']}
+				defaultSelectedKeys={['']}
 				style={{ lineHeight: '64px' }}
 			>
 				<Menu.Item key="1">
@@ -27,30 +26,35 @@ class CustomLayout extends React.Component {
 				</Menu.Item>
 			</Menu>
 		</Header>
-	{/*End Of Header layout*/}
+	{/*End of Header layout*/}
 
-    {/*Beggining of content layout*/}
+    {/*Beggining of Content layout*/}
 		<Content style={{ padding: '0 50px', marginTop: 64 }}>
 			<Breadcrumb style={{ margin: '16px 0' }}>
+	
+			    <Breadcrumb.Separator>
+			          Search By :
+			    </Breadcrumb.Separator>
+
+			    <Breadcrumb.Item>
+		              Movie Name
+		        </Breadcrumb.Item>
+          			
 				<Breadcrumb.Item>
-          			Search By:
-					Movie Name
+				      Genre
 				</Breadcrumb.Item>
 
 				<Breadcrumb.Item>
-		          Genre
-		        </Breadcrumb.Item>
-
-				<Breadcrumb.Item>
-		          Movie Plot
-		        </Breadcrumb.Item>
-			</Breadcrumb>
+				      Movie Plot
+				</Breadcrumb.Item>
+		          
+		    </Breadcrumb>
 
 			<div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
 				{this.props.children}
 			</div>
 		</Content>
-	{/*End Of content layout*/}
+	{/*End of Content layout*/}
 
     {/*Beginning of Footer layout*/}
 		<Footer style={{ textAlign: 'center' }}>SMS ©2019 Created by SMS Tech</Footer>

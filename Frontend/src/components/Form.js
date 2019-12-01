4505766c5df31df4494f1846e4c4689cd59d8761
OluwaseunOjeleye <oluwaseunojeleye@pm.me>
2019-12-01 22:45:50 +0300

    movie service/interface for consuming API from frontend created

--- Frontend/src/components/Form.js
+++ Frontend/src/components/Form.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Form, Input, Button, InputNumber } from "antd";   {/*Importing important form components from antd UI Framework*/}
 
+import  MoviesService  from  "../http_client/MoviesService";
+
 const FormItem = Form.Item;
+
+const  moviesService=new  MoviesService();
+
 
 class CustomForm extends React.Component {
   handleFormSubmit = async (event, requestType, movieID) => {
@@ -9,8 +14,46 @@ class CustomForm extends React.Component {
     const moviename=event.target.elements.moviename.value;
     const director=event.target.elements.director.value;
     const actors=event.target.elements.actors.value;
+    const genres=event.target.elements.genres.value;
     const year=event.target.elements.year.value;
     const plot=event.target.elements.plot.value;
+
+    switch (requestType){
+      //For Creating/ Adding a new movie to database
+      case 'post':
+        moviesService.createMovie(
+          {
+          "title":  moviename,
+          "directors": director,
+          "actors":  actors,
+          "plot":  plot,
+          "year":  year,
+          "genres":  genres
+          }
+        ).then(function (result) {
+            console.log(result);
+            alert("Movie Created!");
+            }
+          );
+
+      //For Updating an already existing movie in database
+      case 'put':
+        moviesService.updateMovie(
+          {
+          "title":  moviename,
+          "directors": director,
+          "actors":  actors,
+          "plot":  plot,
+          "year":  year,
+          "genres":  genres
+          }, movieID).then(function (result) {
+            console.log(result);
+            alert("Movie updated!");
+            }
+          ).catch(()=>{
+            alert('Movie Update Error.');
+          });
+    }
 
   }
 
@@ -18,8 +61,12 @@ class CustomForm extends React.Component {
     return (
     {/*Beginning of the Form*/}
       <div>
-        <Form>
-        <Form onSubmit={event =>this.handleFormSubmit(this.props.requestType, this.props.movieID)}>
+        <Form onSubmit={event =>this.handleFormSubmit(
+            event, 
+            this.props.requestType, 
+            this.props.movieID
+            )}
+        >
 
           <FormItem label="Movie Name">
             <Input name="moviename" placeholder="Add movie name here ..." />
@@ -33,11 +80,15 @@ class CustomForm extends React.Component {
             <Input name="actors" placeholder="Add actors here ..." />
           </FormItem>
 
+          <FormItem label="Genres">
+            <Input name="genres" placeholder="Add genres here ..." />
+          </FormItem>
+
           <FormItem label="Year">
             <InputNumber name="year" placeholder="Add Year here ..." />
           </FormItem>
 
-          <FormItem number="Plot">
+          <FormItem label="Plot">
             <Input name="plot" placeholder="Add Plot here ..." />
           </FormItem>
 
--- Frontend/src/containers/views/MovieDetailView.js
+++ Frontend/src/containers/views/MovieDetailView.js
@@ -2,39 +2,70 @@ import React from "react";
 import { Button, Card } from "antd";             //Importing important tools from antd UI Framework
 import CustomForm from "../../components/Form";  //Importing CustomForm from Form.js file
 
-//movie information data class 
+import  MoviesService  from  "../../http_client/MoviesService";
 
+
+//movie information data class 
 const listData = [];
-  for (let i = 1; i <= 23; i++) {
-    listData.push({
-      href: 'http://smsmovie.com',
-      title: `Movie Name ${i}`,
-      director: 'movie director',
-      actors: 'movie actors',
-      plot: 'movie plot',
-      year: '2019',
-      image: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+for (let i = 1; i <= 23; i++) {
+  listData.push({
+    href: 'http://smsmovie.com',
+    title: `Movie Name ${i}`,
+    director: 'movie director',
+    actors: 'movie actors',
+    plot: 'movie plot',
+    year: '2019',
+    image: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+  });
+}
+
+const  moviesService=new  MoviesService();
+
+class MovieDetail extends React.Component {
+
+  state = {
+    movie: {}
+  };
+
+  //getting data from database
+  componentDidMount() {
+    var self = this;
+    const movieID = self.props.match.params.movieID;
+    moviesService.getMovie(movieID).then(function (result) {
+    console.log(result);
+    self.setState({movie: result});
     });
   }
 
-class MovieDetail extends React.Component {
+  //function for deleting movie from Database
+  handleDelete=(event)=>{
+    var self = this;
+    const movieID = self.props.match.params.movieID;
+    moviesService.deleteMovie(movieID);
+    this.props.history.push(`/movies/`);
+    this.forceUpdate();
+  }
+
   render() {
     return (
       <div>
-//movie information titles  
-        <Card title="Moviename">
-          <p> Director </p>
-          <p> Actors </p>
-          <p> Year </p>
-          <p> Plot </p>
-//Delete & update movie buttons
+        //movie information titles  
+        <Card title={this.state.movie.title}>
+          Director: {this.state.movie.directors}<br/>
+              Actors: {this.state.movie.actors}<br/>
+              Genre: {this.state.movie.genres}<br/>
+              Year: {this.state.movie.year}<br/>
+              Plot: {this.state.movie.plot}
         </Card>
-        <CustomForm requestType="put" articleID={null} btnText="Update"/>        
-        <form>
+
+        //Delete & Update movie buttons
+        <CustomForm requestType="put" movieID={this.props.match.params.movieID} btnText="Update"/>
+        <form onSubmit={this.handleDelete}>
           <Button type="danger" htmlType="submit">
             Delete
           </Button>
         </form>
+
       </div>
     );
   }

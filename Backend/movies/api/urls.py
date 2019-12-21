0e8ddf0bd227f24a2773ce8adf2f74861ab6592d
OluwaseunOjeleye <oluwaseunojeleye@pm.me>
2019-12-22 02:00:02 +0300

    Update-Recommendation System Integration with Backend

--- Backend/movies/api/urls.py
+++ Backend/movies/api/urls.py
@@ -13,15 +13,22 @@ from .userViews import (
     MovieUserDetailView as userMovieDetailView,
 )
 
+from . import userViews
+
+
 urlpatterns = [
-    ## Admin pathS
+    ## Admin URLS
     path('', adminMovieListView.as_view()),
     path('<pk>', adminMovieDetailView.as_view()),
     path('create/', adminMovieCreateView.as_view()),
     path('<pk>/update/', adminMovieUpdateView.as_view()),
     path('<pk>/delete/', adminMovieDeleteView.as_view()),
 
-    ##User pathS
+    ##User URLS
     path('result/', userMovieListView.as_view()),
     path('result/<pk>', userMovieDetailView.as_view()),
+
+    ##Suggest
+    path('suggest/', userViews.get_UserData),
+
 ]
--- Backend/movies/api/userViews.py
+++ Backend/movies/api/userViews.py
@@ -1,18 +1,74 @@
-from rest_framework import permissions
 
+from rest_framework import permissions
+from rest_framework.response import Response
 from rest_framework.generics import (
-    ListAPIView,
     RetrieveAPIView,
 )
 
 from ..models import Movie
 from .serializers import MovieSerializer
 
+import pandas as pd
+import datetime
+import os
+from ..RecommendationSystem import recommend_v3 as Recommend
+from django.db.models import Case, When
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
 
-class MovieUserListView(ListAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-    #permission_classes = (permissions.AllowAny, )
+
+#global variable for storing the ids of movie to recommend
+recommendation_movie_ids=[] 
+
+#Method for listening to User's POST request
+@csrf_exempt
+def get_UserData(request):
+    if request.method=="POST":
+        data=request.POST
+
+        ##getting user's inputs
+        search_type=data.get("searchtype", "0")
+        movie_name=data.get("moviename", "0")
+        no_of_movies=int(data.get("numberofmovies", "0"))
+        
+        global recommendation_movie_ids
+        recommendation_movie_ids=suggest_Movies(search_type, movie_name, no_of_movies)
+
+        if(len(recommendation_movie_ids)!=0):
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=400)
+
+
+# Function That calls The Movie Recommending System
+def suggest_Movies(search_type, movie_name, no_of_movies):
+    #querying all movies info from the database
+    movie_table= pd.DataFrame(list(Movie.objects.all().values('title', 'genres', 'directors', 'actors', 'plot')))
+    movies_id_no=pd.DataFrame(list(Movie.objects.all().values('id')))
+
+    #Creating Recommendation System object
+    Recommender=Recommend.MovieRS(search_type, movie_table, movies_id_no)
+    #getting recommended movies from recommendation system
+    recommended_movie_ids=Recommender.Recommend(movie_name, no_of_movies)
+
+    if(len(recommended_movie_ids)!=0):
+        print(recommended_movie_ids)
+        return recommended_movie_ids
+    else:
+        return []
+
+
+#Result List API View Class- for getting the recommended 
+""" movies details from db and sending the result in JSON format to endpoint API api/results"""
+class MovieUserListView(APIView):
+    def get(self, request):
+        global recommendation_movie_ids
+        preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(recommendation_movie_ids)])
+        queryset = Movie.objects.filter(pk__in=recommendation_movie_ids).order_by(preserved)
+        serializer_class = MovieSerializer(queryset, many=True)
+        return Response(serializer_class.data)
 
 
 class MovieUserDetailView(RetrieveAPIView):
